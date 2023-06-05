94fe1971a99b1d2c136dbd6f6fe403e938c09fcc
farhan00hm <vectorit.farhan@gmail.com>
2023-06-06 01:41:29 +0600

    working on party wise

--- resources/views/party-wise.blade.php
+++ resources/views/party-wise.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panchagarh Election Data</title>
+    <title>Panchagarh</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <style>
@@ -59,20 +59,41 @@
             transition: 0.3s;
         }
 
+        .rectangle {
+            width: 10px;
+            height: 10px;
+            display: inline-block;
+            margin-right: 5px;
+        }
+
+        .red {
+            background-color: red;
+        }
+
+        .green {
+            background-color: green;
+        }
     </style>
 </head>
 <body>
 <div id="svg-container">
-    <span style="text-align: center">Panchagarh-1 Election Data</span>
+    <span style="text-align: center">Panchagarh-1</span>
 
     <div id="button-container">
-        <div class="button-group" data-group="year">
+        <div class="button-group" data-group="party">
             <label for="year">Party:</label>
-            <input type="radio" name="party" id="bnp" value="bnp" checked>
+            <input type="radio" name="party" id="bnp" value="ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির" checked>
             <label for="bnp">BNP</label>
-            <input type="radio" name="party" id="awamileague" value="awamileague">
+            <input type="radio" name="party" id="awamileague" value="মোঃ_মজাহারুল_হক_প্রধান">
             <label for="awamileague">Awami League</label>
         </div>
+    </div>
+    <br>
+    <div class="button-group" data-group="year">
+        <div class="rectangle red"></div>
+        <label for="year-2008">2008</label>
+        <div class="rectangle green"></div>
+        <label for="year-2018">2018</label>
     </div>
 
 
@@ -108,53 +129,54 @@
 
             @foreach ($pathsValue as $key=>$value)
                 @php
-                    $desiredYear = '2008';
-$desiredUnion = $key;
+                    $year2008 = '2008';
+                    $year2018 = '2018';
+                    $desiredUnion = $key;
 
-$filteredDataOf2008 = $datas->filter(function ($object) use ($desiredYear, $desiredUnion) {
-    return $object->সাল === '2008' && $object->ইউনিয়ন === $desiredUnion;
-});
-                        $dataOf2008 = $filteredDataOf2008->first();
-                        $filteredDataOf2018 = $datas->filter(function ($object) use ($desiredYear, $desiredUnion) {
-    return $object->সাল === '2018' && $object->ইউনিয়ন === $desiredUnion;
-});
-                        $dataOf2018 = $filteredDataOf2018->first();
+                    $filteredDataOf2008 = $datas->filter(function ($object) use ($year2008, $desiredUnion) {
+                        return $object->সাল === '2008' && $object->ইউনিয়ন === $desiredUnion;
+                    });
+                    $dataOf2008 = $filteredDataOf2008->first();
+                    $filteredDataOf2018 = $datas->filter(function ($object) use ($year2018, $desiredUnion) {
+                    return $object->সাল === '2018' && $object->ইউনিয়ন === $desiredUnion;
+                    });
+                    $dataOf2018 = $filteredDataOf2018->first();
 
-                            $percentageOf2008 = ($dataOf2008->ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির * 100)/$dataOf2008->মোট_বৈধ;
-                            $percentageOf2018 = ($dataOf2018->ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির * 100)/$dataOf2018->মোট_বৈধ;
+                    $percentageOf2008 = round(($dataOf2008->ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির * 100)/$dataOf2008->মোট_বৈধ,2);
+                    $percentageOf2018 = round(($dataOf2018->ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির * 100)/$dataOf2018->মোট_বৈধ,2);
 
-                    @endphp
+                @endphp
                 <defs>
 
-                        <linearGradient id="grad{{$key}}" x1="0%" y1="100%" x2="0%" y2="0%">
-                            <stop offset="0%" style="stop-color: red; stop-opacity: 1"/>
-                            <stop offset="{{ $percentageOf2008 }}%" style="stop-color: red; stop-opacity: 1"/>
-                            <stop offset="{{ $percentageOf2008 }}%" style="stop-color: green; stop-opacity: 1"/>
-                            <stop offset="{{ $percentageOf2008 + $percentageOf2018 }}%"
-                                  style="stop-color: green; stop-opacity: 1"/>
-                            <stop offset="{{ $percentageOf2008 + $percentageOf2018 }}%"
-                                  style="stop-color: #fff; stop-opacity: 1"/>
-                            <stop offset="100%" style="stop-color: #fff; stop-opacity: 1"/>
-                        </linearGradient>
-                    </defs>
+                    <linearGradient id="grad{{$key}}" x1="0%" y1="100%" x2="0%" y2="0%">
+                        <stop offset="0%" style="stop-color: red; stop-opacity: 1"/>
+                        <stop offset="{{ $percentageOf2008 }}%" style="stop-color: red; stop-opacity: 1"/>
+                        <stop offset="{{ $percentageOf2008 }}%" style="stop-color: green; stop-opacity: 1"/>
+                        <stop offset="{{ $percentageOf2008 + $percentageOf2018 }}%"
+                              style="stop-color: green; stop-opacity: 1"/>
+                        <stop offset="{{ $percentageOf2008 + $percentageOf2018 }}%"
+                              style="stop-color: #fff; stop-opacity: 1"/>
+                        <stop offset="100%" style="stop-color: #fff; stop-opacity: 1"/>
+                    </linearGradient>
+                </defs>
                 <path
-                        class="path-hover"
-                        d="{{ $value }}"
-                        data-union="{{ $key }}"
-                        data-a="{{ $percentageOf2008 }}"
-                        data-b="{{ $percentageOf2018 }}"
-                        style="opacity:0.5;fill: url(#grad{{$key}}); ;fill-opacity:0.6;stroke:#2c2c2f;stroke-width:0.22900671;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.9372549"
-                        {{--                    style="opacity:0.5;fill: {{ $data->ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির > $data->মোঃ_মজাহারুল_হক_প্রধান ? 'red' : 'green' }} ;fill-opacity:0.6;stroke:#2c2c2f;stroke-width:0.22900671;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.9372549"--}}
-                    />
+                    class="path-hover"
+                    d="{{ $value }}"
+                    data-union="{{ $key }}"
+                    data-a="{{ $percentageOf2008 }}"
+                    data-b="{{ $percentageOf2018 }}"
+                    style="opacity:0.5;fill: url(#grad{{$key}}); ;fill-opacity:0.6;stroke:#2c2c2f;stroke-width:0.22900671;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.9372549"
+                    {{--                    style="opacity:0.5;fill: {{ $data->ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির > $data->মোঃ_মজাহারুল_হক_প্রধান ? 'red' : 'green' }} ;fill-opacity:0.6;stroke:#2c2c2f;stroke-width:0.22900671;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.9372549"--}}
+                />
             @endforeach
         </g>
     </svg>
 
     <div class="hover-info">
         <h3 style="text-align: center" id="আসন"></h3>
-        <span>2008: <p id="jomir" style="display: inline"> </p> </span>
+        <span>2008: <p id="jomir" style="display: inline"> </p> %</span>
         <br>
-        <span>2018: <p id="majharul" style="display: inline"></p></span>
+        <span>2018: <p id="majharul" style="display: inline"></p> %</span>
     </div>
 </div>
 
@@ -162,19 +184,16 @@ $filteredDataOf2008 = $datas->filter(function ($object) use ($desiredYear, $desi
     $(document).ready(function () {
 
         // Filter button click event
-        $('.button-group[data-group="year"] input[type="radio"]').on('change', function () {
-            var selectedYear = $(this).val();
+        $('.button-group[data-group="party"] input[type="radio"]').on('change', function () {
+            let selectedParty = $(this).val();
 
             // Filter the data based on the selected year
-            var datas = {!! $datas !!};
-            var pathvalues = @json($pathsValue);
-            var filteredData = datas.filter(function (data) {
-                return data['সাল'] === selectedYear;
-            });
+            let datas = {!! $datas !!};
+            let pathvalues = @json($pathsValue);
+
 
             // Generate the updated SVG code
-            var svgCode = '<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" id="panchagarh-1-map" version="1.1" viewBox="0 0 210 297" height="250mm" width="210mm">';
-
+            let svgCode = '<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" id="panchagarh-1-map" version="1.1" viewBox="0 0 210 297" height="250mm" width="210mm">';
             svgCode += '<defs id="defs558"></defs>';
             svgCode += '<metadata id="metadata561">';
             svgCode += '<rdf:RDF>';
@@ -187,32 +206,36 @@ $filteredDataOf2008 = $datas->filter(function ($object) use ($desiredYear, $desi
             svgCode += '</metadata>';
             svgCode += '<g id="layer1">';
 
-            filteredData.forEach(function (data, index) {
+            datas.forEach(function (data, index) {
                 let union = data['ইউনিয়ন'];
-                // var color = data['ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির'] > data['মোঃ_মজাহারুল_হক_প্রধান'] ? 'red' : 'green';
-                var bnpPercentage = (data['ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির'] * 100) / data['মোট_বৈধ'];
-                var awamiPercentage = (data['মোঃ_মজাহারুল_হক_প্রধান'] * 100) / data['মোট_বৈধ'];
-                console.log("Union: " + union + " Jamir: " + data['ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির'] + " Majharul: " + data['মোঃ_মজাহারুল_হক_প্রধান'] + " Bnp: " + bnpPercentage + " Awami: " + awamiPercentage)
+
+                // Filter the data for 2008
+                var filteredData2008 = datas.filter(function (data) {
+                    return data['সাল'] === '2008' && data['ইউনিয়ন'] === union;
+                });
+                var dataOf2008 = filteredData2008[0];
+                var percentageOf2008 = ((dataOf2008[selectedParty] * 100) / dataOf2008['মোট_বৈধ']);
+
+
+                // Filter the data for 2018
+                var filteredData2018 = datas.filter(function (data) {
+                    return data['সাল'] === '2018' && data['ইউনিয়ন'] === union;
+                });
+                var dataOf2018 = filteredData2018[0];
+                var percentageOf2018 = ((dataOf2018[selectedParty] * 100) / dataOf2018['মোট_বৈধ']);
 
 
                 svgCode += '<defs>';
                 svgCode += '<linearGradient id="grad' + index + '" x1="0%" y1="100%" x2="0%" y2="0%">';
                 svgCode += '<stop offset="0%" style="stop-color: red; stop-opacity: 1" />';
-                svgCode += '<stop offset="' + bnpPercentage + '%" style="stop-color: red; stop-opacity: 1" />';
-                svgCode += '<stop offset="' + bnpPercentage + '%" style="stop-color: green; stop-opacity: 1" />';
-                svgCode += '<stop offset="' + (bnpPercentage + awamiPercentage) + '%" style="stop-color: green; stop-opacity: 1" />';
-                svgCode += '<stop offset="' + (bnpPercentage + awamiPercentage) + '%" style="stop-color: white; stop-opacity: 1" />';
+                svgCode += '<stop offset="' + percentageOf2008 + '%" style="stop-color: red; stop-opacity: 1" />';
+                svgCode += '<stop offset="' + percentageOf2008 + '%" style="stop-color: green; stop-opacity: 1" />';
+                svgCode += '<stop offset="' + (percentageOf2008 + percentageOf2018) + '%" style="stop-color: green; stop-opacity: 1" />';
+                svgCode += '<stop offset="' + (percentageOf2008 + percentageOf2018) + '%" style="stop-color: white; stop-opacity: 1" />';
                 svgCode += '<stop offset="100%" style="stop-color: white; stop-opacity: 1" />';
                 svgCode += '</linearGradient>';
-                // svgCode += '<linearGradient id="grad' + index + '" x1="0%" y1="100%" x2="0%" y2="0%">';
-                // svgCode += '<stop offset="0%" style="stop-color: red; stop-opacity: 1" />';
-                // svgCode += '<stop offset="' + bnpPercentage + '%" style="stop-color: red; stop-opacity: 1" />';
-                // svgCode += '<stop offset="' + awamiPercentage + '%" style="stop-color: green; stop-opacity: 1" />';
-                // // svgCode += '<stop offset="' + (bnpPercentage + awamiPercentage + remainingPercentage) + '%" style="stop-color: #fff; stop-opacity: 1" />';
-                // svgCode += '<stop offset="100%" style="stop-color: #fff; stop-opacity: 1" />';
-                // svgCode += '</linearGradient>';
                 svgCode += '</defs>';
-                svgCode += '<path class="path-hover" d="' + pathvalues[union] + '" data-union="' + data['ইউনিয়ন'] + '" data-a="' + data['ব্যারিস্টার_মুহম্মদ_নওশাদ_জমির'] + '" data-b="' + data['মোঃ_মজাহারুল_হক_প্রধান'] + '" style="opacity:0.5;fill: url(#grad' + index + '); fill-opacity:0.6; stroke:#2c2c2f; stroke-width:0.22900671; stroke-linecap:round; stroke-linejoin:bevel; stroke-miterlimit:4; stroke-dasharray:none; stroke-dashoffset:0; stroke-opacity:0.9372549" />';
+                svgCode += '<path class="path-hover" d="' + pathvalues[union] + '" data-union="' + data['ইউনিয়ন'] + '" data-a="' + percentageOf2008 + '" data-b="' + percentageOf2018 +  '" style="opacity:0.5;fill: url(#grad' + index + '); fill-opacity:0.6; stroke:#2c2c2f; stroke-width:0.22900671; stroke-linecap:round; stroke-linejoin:bevel; stroke-miterlimit:4; stroke-dasharray:none; stroke-dashoffset:0; stroke-opacity:0.9372549" />';
             });
 
             svgCode += '</g>';
@@ -223,17 +246,7 @@ $filteredDataOf2008 = $datas->filter(function ($object) use ($desiredYear, $desi
         });
 
 
-        // Button group click event
-        $('.button-group .button').click(function () {
-            $(this).addClass('active').siblings().removeClass('active');
 
-            // Get selected values
-            var selectedParty = $('.button-group[data-group="party"] .button.active').data('party');
-            var selectedYear = $('.button-group[data-group="year"] .button.active').data('year');
-
-            // Perform actions based on selected values
-            // ...
-        });
 
         $('#svg-container').on('mouseenter', '.path-hover', function () {
             let jomir = $(this).data('a');
